47b958608cb946936c0d712a08e858be7eb899ef
Al-Owal <billy_wintergreen@protonmail.com>
2023-01-09 11:47:32 +0300

    Create a function for random computer selection

--- javascript.js
+++ javascript.js
@@ -1,10 +1,23 @@
-function getComputerChoice {
+function getComputerChoice() {
+    function random_items(items) {
+        return items[Math.floor(Math.random()*items.length)];
+    }    
 
+    const items = ['rock', 'paper', 'scissor'];
+    console.log(random_items(items));
 }
 
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+getComputerChoice();
+
+/*
 function game() {
 
-}
+}*/
 
+/*
 const playerSelection;
-const computerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();*/
